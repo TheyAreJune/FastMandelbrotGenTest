7027b037579de3a5c5ceaec03beec5387295edda
June P <TheyAreJune@gmail.com>
2021-06-02 01:07:22 -0400

    Initialize File, import libraries

---
+++ gen.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Drawing.Imaging;
+
+namespace FastFractalTest
+{
+    class RoughDraft
+    {
+        static void Main(string[] args)
+        {
+            /* Placeholder Lines
+             * Yep
+             */
+        }
+    }
+}
